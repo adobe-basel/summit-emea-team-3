2a2fc6492a12daba28699dbf9fa50adb5433abe5
Niek Raaijmakers <raaijmak@adobe.com>
2023-05-11 09:52:37 +0200

    Update footer.js
    fix linting

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,6 +1,6 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
-const blankSuffix = '_blank'
+const blankSuffix = '_blank';
 
 function fixElements(element) {
   // Check if the element is a link and its text ends with '_blank'
@@ -17,33 +17,29 @@ function fixElements(element) {
     element.children[0].append(placeholder);
   }
 
-
   if (element.classList.contains('footer-right')) {
     const placeholder = document.createElement('div');
     placeholder.classList.add('placeholder');
     element.children[0].prepend(placeholder);
   }
 
-
   if (element.classList.contains('footer-right')) {
-    const headerEls = element.querySelectorAll(":scope > div > div > p > strong > a");
-    for (const headerEl of headerEls) {
-      headerEl.classList.add("button", "secondary");
-    };
+    element.querySelectorAll(':scope > div > div > p > strong > a').forEach((headerEl) => {
+      headerEl.classList.add('button', 'secondary');
+    });
   }
 
-  if(element.classList.contains('footer-half')) {
-    const headerEls = element.querySelectorAll(":scope > div > div > h3");
-    for(const headerEl of headerEls) {
-      headerEl.addEventListener("click", function() {
+  if (element.classList.contains('footer-half')) {
+    element.querySelectorAll(':scope > div > div > h3').forEach((headerEl) => {
+      headerEl.addEventListener('click', () => {
         headerEl.parentElement.classList.toggle('expanded');
       });
-    }
+    });
   }
 
   if (!element.children.length) return;
 
-  for (let i = 0; i < element.children.length; i++) {
+  for (let i = 0; i < element.children.length; i += 1) {
     const child = element.children[i];
 
     fixElements(child);
@@ -54,24 +50,24 @@ function fixElements(element) {
   * loads and decorates the footer
   * @param {Element} block The footer block element
   */
-  export default async function decorate(block) {
-    const cfg = readBlockConfig(block);
-    block.textContent = '';
+export default async function decorate(block) {
+  const cfg = readBlockConfig(block);
+  block.textContent = '';
 
-    // fetch footer content
-    const footerPath = cfg.footer || '/footer';
-    const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+  // fetch footer content
+  const footerPath = cfg.footer || '/footer';
+  const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
 
-    if (resp.ok) {
-      const html = await resp.text();
+  if (resp.ok) {
+    const html = await resp.text();
 
-      // decorate footer DOM
-      const footer = document.createElement('div');
-      footer.classList.add('footer-container');
-      footer.innerHTML = html;
-      fixElements(footer);
+    // decorate footer DOM
+    const footer = document.createElement('div');
+    footer.classList.add('footer-container');
+    footer.innerHTML = html;
+    fixElements(footer);
 
-      decorateIcons(footer);
-      block.append(footer);
-    }
+    decorateIcons(footer);
+    block.append(footer);
   }
+}
