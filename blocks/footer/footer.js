e3b4854fdd9c43d3570c72507ce0b4b7de997d90
Aliaksandra Kupreyeva <akupreyeva@adobe.com>
2023-05-11 07:58:17 +0200

    Add footer block

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,25 +1,77 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
-/**
- * loads and decorates the footer
- * @param {Element} block The footer block element
- */
-export default async function decorate(block) {
-  const cfg = readBlockConfig(block);
-  block.textContent = '';
+const blankSuffix = '_blank'
 
-  // fetch footer content
-  const footerPath = cfg.footer || '/footer';
-  const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+function fixElements(element) {
+  // Check if the element is a link and its text ends with '_blank'
+  if (element.tagName === 'A' && element.textContent.trim().endsWith(blankSuffix)) {
+    element.textContent = element.textContent.trim().slice(0, -blankSuffix.length);
 
-  if (resp.ok) {
-    const html = await resp.text();
+    // Modify the link to open in a new window
+    element.target = '_blank';
+  }
 
-    // decorate footer DOM
-    const footer = document.createElement('div');
-    footer.innerHTML = html;
+  if (element.classList.contains('footer-left')) {
+    const placeholder = document.createElement('div');
+    placeholder.classList.add('placeholder');
+    element.children[0].append(placeholder);
+  }
 
-    decorateIcons(footer);
-    block.append(footer);
+
+  if (element.classList.contains('footer-right')) {
+    const placeholder = document.createElement('div');
+    placeholder.classList.add('placeholder');
+    element.children[0].prepend(placeholder);
+  }
+
+
+  if (element.classList.contains('footer-right')) {
+    const headerEls = element.querySelectorAll(":scope > div > div > p > strong > a");
+    for (const headerEl of headerEls) {
+      headerEl.classList.add("button", "secondary");
+    };
+  }
+
+  if(element.classList.contains('footer-half')) {
+    const headerEls = element.querySelectorAll(":scope > div > div > h3");
+    for(const headerEl of headerEls) {
+      headerEl.addEventListener("click", function() {
+        headerEl.parentElement.classList.toggle('expanded');
+      });
+    }
+  }
+
+  if (!element.children.length) return;
+
+  for (let i = 0; i < element.children.length; i++) {
+    const child = element.children[i];
+
+    fixElements(child);
   }
 }
+
+/**
+  * loads and decorates the footer
+  * @param {Element} block The footer block element
+  */
+  export default async function decorate(block) {
+    const cfg = readBlockConfig(block);
+    block.textContent = '';
+
+    // fetch footer content
+    const footerPath = cfg.footer || '/footer';
+    const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+
+    if (resp.ok) {
+      const html = await resp.text();
+
+      // decorate footer DOM
+      const footer = document.createElement('div');
+      footer.classList.add('footer-container');
+      footer.innerHTML = html;
+      fixElements(footer);
+
+      decorateIcons(footer);
+      block.append(footer);
+    }
+  }
